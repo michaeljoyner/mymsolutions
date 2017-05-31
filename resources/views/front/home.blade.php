e00e4577c4fd0efe231f7bc433442a671343b29a
Michael Joyner <joyner.michael@gmail.com>
2017-05-31 10:52:39 +0800

    update text for about and new service sections

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -12,26 +12,26 @@
     </section>
     <section id="about" class="about-section hp-section">
         <h2 class="section-title">ABOUT</h2>
-        <p>You already know this but we'll say it anyway - to be taken seriously in your industry you need to look the part.You've got the ideas and you've got the talent, now all you need is an awesome identity to make your presence truly felt. Agreed? Cool, then keep reading.</p>
-        <p>Make Your Mark (that's us) wants to take your new or existing brand and make it look remarkable. We want to give it the personality it deserves, while maintaining stature and functionality. That's not always an easy balance to achieve, but don't worry, that's what we are here for.</p>
-        <p>We specialise in logo design, corporate branding and web design and development.</p>
+        <p>You already know this but we’ll say it anyway - if you want to take your business to another level then having an online presence is essential. Agreed? Cool, then keep reading.</p>
+        <p>Google processes over 40,000 search queries per second and Facebook has almost 2 billion active users. Make Your Mark (that’s us) will guide you in creating successful digital marketing campaigns ensuring that you stay ahead of your competition.</p>
+        <p>We are specialists in Google advertising, social media management and web design and development.</p>
     </section>
     <section id="services" class="service-section hp-section">
         <h2 class="section-title">SERVICES</h2>
         <div class="service-box-wrapper">
             <div class="service-box">
                 <h3><i class="fa fa-paint-brush"></i></h3>
-                <h3>LOGOS</h3>
-                <p>Your logo is the face of your organisation. People will look at it and judgement will instantly be made about you. For this reason your logo should represent what you do, why you do it and how you do it in a captivating, emotional, timeless way. Sounds complex, right? It is. But don't worry, we've got this. We take great pride in our logo craft and want nothing more than to help you make your everlasting mark in this world.</p>
+                <h3>Social Media Management</h3>
+                <p>The numbers speak for themselves. The amount of people using Social media in one form or another is staggering. Can your business afford to be left out? With the ability to target your audience, increase brand awareness and loyalty, get real-time results from campaigns and provide rich customer experiences a successful Social media strategy is essential to any business small or big.</p>
             </div>
             <div class="service-box">
                 <h3><i class="fa fa-group"></i></h3>
-                <h3>IDENTITY</h3>
-                <p>It's time to add some personality to your identity. You want to reach out to people and let them know who you are what you do. For this your branding needs to be clear, presentable and informative with just the right amount of charm. We offer superior branding identity solutions, from the all-important business card, to corporate stationery, clothing and gifting, to print and digital layout design. </p>
+                <h3>Google<br>Adwords</h3>
+                <p>No matter what your business does, thousands of people are searching for your product or service every day online. Google Advertising is an online advertising service that ensures that your business gains the maximum amount of exposure online by analyzing these search queries and creating appropriate Google advertisements keeping your business ahead of the pack.</p>
             </div>
             <div class="service-box">
                 <h3><i class="fa fa-gears"></i></h3>
-                <h3>WEB DESIGN</h3>
+                <h3>WEB<br>DESIGN</h3>
                 <p>Need to connect? Then don't kid yourself, you need a website. No brand is complete without a web presence, and you will only benefit from this. From a simple landing page with your contact details to a multi-faceted, outright work horse, we can create the website you need to showcase your company's products, services and talent, making it easier for people to find you, and for you to stand out.</p>
             </div>
         </div>
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,22 +1,20 @@
 <footer class="site-footer">
     <nav class="footer-nav">
         <ul>
-            <li><i class="fa fa-home fa-2x"></i>
-            @if(Request::path() === 'home' || Request::path() === '/')
-                <a href="#">back to top</a>
-            @else
-                <a href="{{ url('/') }}">home</a>
-            @endif
+            <li>
+                <a href="#">
+                    <i class="fa fa-home fa-2x"></i>
+                    back to top</a>
             </li>
 
-            <li><i class="fa fa-envelope fa-2x"></i>
-            @if(Request::path() === 'home' || Request::path() === '/')
-                <a href="#contact">contact us</a>
-            @else
-                <a href="{{ url('/#contact') }}">contact us</a>
-            @endif
-            </li>
+            {{--<li><i class="fa fa-envelope fa-2x"></i>--}}
+            {{--@if(Request::path() === 'home' || Request::path() === '/')--}}
+                {{--<a href="#contact">contact us</a>--}}
+            {{--@else--}}
+                {{--<a href="{{ url('/#contact') }}">contact us</a>--}}
+            {{--@endif--}}
+            {{--</li>--}}
         </ul>
     </nav>
-    <p>&copy; Copyright Reserved 2015</p>
+    <p>&copy; Copyright Reserved {{ \Carbon\Carbon::now()->year }}</p>
 </footer>
